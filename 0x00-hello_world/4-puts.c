2fdf01be9184907625261a294c30b214dfbcbec4
KipronohVincent <kipronohvincent43@gmail.com>
2022-03-10 12:42:11 +0300

    deed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ *  Return: Always 0 (Sucess)
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
