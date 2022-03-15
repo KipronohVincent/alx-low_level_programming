544529b693720a639dae6b2cc5ebb3a122657004
KipronohVincent <kipronohvincent43@gmail.com>
2022-03-15 03:06:59 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
