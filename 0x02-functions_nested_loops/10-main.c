0bba63d53a9024b8c6fee49248377ca72efccc93
KipronohVincent <kipronohvincent43@gmail.com>
2022-03-15 03:31:31 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
