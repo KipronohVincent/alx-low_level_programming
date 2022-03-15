9abb6cea17793d636ee82b689df5b861ab48aff3
KipronohVincent <kipronohvincent43@gmail.com>
2022-03-15 03:02:54 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
