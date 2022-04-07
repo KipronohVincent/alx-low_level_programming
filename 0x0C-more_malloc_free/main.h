512a27d04947f0b5b5ff69342fe0c2a3357aece3
KipronohVincent <kipronohvincent43@gmail.com>
2022-04-07 03:01:09 -0700

    positive numbers.

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
