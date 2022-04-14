be079d9b7d0030e9777fb7f5856bab13e6abcf6c
KipronohVincent <kipronohvincent43@gmail.com>
2022-04-14 03:04:01 -0700

    header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,9 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-#endif 
+#endif
