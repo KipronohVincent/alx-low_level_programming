d4dad8f38cdae391809039b53602326066c288a2
KipronohVincent <kipronohvincent43@gmail.com>
2022-04-28 01:56:26 -0700

    Tests

---
+++ 0x14-bit_manipulation/tests/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_binary(0);
+    printf("\n");
+    print_binary(1);
+    printf("\n");
+    print_binary(98);
+    printf("\n");
+    print_binary(1024);
+    printf("\n");
+    print_binary((1 << 10) + 1);
+    printf("\n");
+    return (0);
+}
