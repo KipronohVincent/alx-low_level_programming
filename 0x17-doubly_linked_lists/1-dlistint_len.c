95956453c5a061e4d8b55c9a7d5402494549744c
KipronohVincent <kipronohvincent43@gmail.com>
2022-06-09 13:06:27 +0300

    test

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,13 +9,13 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t i = 0;
-    const dlistint_t *tmp = h;
+size_t i = 0;
+const dlistint_t *tmp = h;
 
 while (tmp != NULL)
 {
-    tmp = tmp->next;
-    i++;
+tmp = tmp->next;
+i++;
 }
 return (i);
 
