6b00024aab13a88e7bcbb9aa4db0515de7e11d36
KipronohVincent <kipronohvincent43@gmail.com>
2022-06-09 12:56:25 +0300

    test

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Returns the number of elements in a dlistint_t list.
+ * @h: The head of the dlistint_t list.
+ * Return: The number of elements in the list.
+ * 
+ * Description: This function returns the number of elements in a dlistint_t
+ * 
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t i = 0;
+    const dlistint_t *tmp = h;
+
+    while (tmp != NULL)
+    {
+        tmp = tmp->next;
+        i++;
+    }
+    return (i);
+}
