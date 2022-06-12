c35a3c99e9a5ee50c9803ca6919e3a34346ea3a2
KipronohVincent <kipronohvincent43@gmail.com>
2022-06-12 15:30:52 +0300

    Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/operand.c
@@ -0,0 +1,22 @@
+#include "operations.h"
+
+int add(int a,int b)
+{
+	return (a + b);
+}
+int sub(int a, int b)
+{
+	return (a - b);
+}
+int mul(int a, int b)
+{
+	return (a * b);
+}
+int div(int a, int b)
+{
+	return (a / b);
+}
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,10 @@
+#ifndef _OPERATIONS_H
+#define _OPERATIONS_H
+
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+#endif
