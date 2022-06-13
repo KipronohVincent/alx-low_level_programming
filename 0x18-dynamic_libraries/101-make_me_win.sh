775a7196b992cfb7ce978f68c6925cdf04b7c441
KipronohVincent <kipronohvincent43@gmail.com>
2022-06-13 10:43:03 +0300

    shared lib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P /tmp 
+wget -P /tmp https://raw.githubusercontent.com/KipronohVincent/alx-low_level_programming/master/0x18-dynamic_libraries/101-make_me_win.sh/libwin.so
+export LD_PRELOAD=/tmp/libwin.so 
